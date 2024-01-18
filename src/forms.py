ba775cfb5bc719e5c2991c41337cf3febdf88c40
francop <franco.pradelli94@gmail.com>
2024-01-18 11:18:00 +0100

    Added division between stalk cells (alpha_p) and endothelial cells (alpha_pc) proliferation

--- src/forms.py
+++ src/forms.py
@@ -119,3 +119,188 @@ def chan_hillard_free_enery(mu: Function):
     energy = - (ufl.grad(mu) ** 2) * ufl.dx
 
     return energy
+
+
+def vascular_proliferation_form(alpha_p,
+                                af: dolfinx.fem.Function,
+                                af_p,
+                                c: dolfinx.fem.Function or ufl.variable.Variable,
+                                v: ufl.TestFunction):
+    r"""
+    Returns the UFL Form for the proliferation term of the vascular tissue as defined by the paper of Travasso et al.
+    (2011) :cite:`Travasso2011a`.
+
+    The corresponding term of the equation is (H is the Heaviside function):
+
+    .. math::
+       \alpha_p(af_p) \cdot c \cdot H(c)
+
+    Where :math: `af` is the angiogenic factor concentration, and :math: `\alpha_p(af)` represents the proliferation
+    rate, that is defined as the follwing function of :math: `af`. The definition of the latter function is the
+    following:
+
+    .. math::
+       \alpha_p(af) &= \alpha_p \cdot af_p \quad \textrm{if} \quad af>af_p \\
+                    &= \alpha_p \cdot af  \quad \textrm{if} \quad 0<af \le af_p \\
+                    & = 0 \quad \textrm{if} \quad af \le 0
+
+    Where :math: `\alpha-p` and :math: `af_p` are constants.
+
+    :param alpha_p: costant of the proliferation rate function for the capillaries
+    :param af: FEniCS function representing the angiogenic factor distribution
+    :param af_p: maximum concentration of angiogenic factor leading to proliferation. If af > af_p, the proliferation
+        rate remains alpha_p * af_p
+    :param c: FEniCS function representing the capillaries
+    :param v: FEniCS test function
+    :return: the UFL form for the proliferation term
+    """
+    # def the proliferation function
+    proliferation_function = alpha_p * af
+    # def the max value for the proliferation function
+    proliferation_function_max = alpha_p * af_p
+    # take the bigger between the two of them
+    proliferation_function_hysteresis = ufl.conditional(ufl.gt(proliferation_function,
+                                                               proliferation_function_max),
+                                                        proliferation_function_max,
+                                                        proliferation_function)
+    # multiply the proliferation term with the vessel field
+    proliferation_term = proliferation_function_hysteresis * c
+    # take it oly if bigger than 0
+    proliferation_term_heaviside = ufl.conditional(ufl.gt(proliferation_term, 0.),
+                                                   proliferation_term,
+                                                   0.)
+    # build the form
+    proliferation_term_form = proliferation_term_heaviside * v * ufl.dx
+    return proliferation_term_form
+
+
+def cahn_hillard_form(c,
+                      c0: dolfinx.fem.Function,
+                      mu: dolfinx.fem.Function,
+                      mu0: dolfinx.fem.Function,
+                      q: dolfinx.fem.Function,
+                      v: dolfinx.fem.Function,
+                      dt,
+                      theta,
+                      chem_potential,
+                      lmbda,
+                      M):
+    r"""
+    Returns the UFL form of a for a general Cahn-Hillard equation, discretized in time using the theta method. The
+    method is the same reported by the FEniCS team in one of their demo `1. Cahn-Hillard equation`_ and is briefly
+    discussed below for your conveneince.
+
+    .. _1. Cahn-Hillard equation:
+       https://fenicsproject.org/olddocs/dolfin/2016.2.0/cpp/demo/documented/cahn-hilliard/cpp/documentation.html
+
+    The Cahn-Hillard equation reads as follows:
+
+    .. math::
+       \frac{\partial c}{\partial t} - \nabla \cdot M (\nabla(\frac{d f}{d c}
+             - \lambda \nabla^{2}c)) = 0 \quad \textrm{in} \ \Omega
+
+    Where :math: `c` is the unknown field to find, :math: `f` is some kind of energetic potential which defines the
+    phase separation, and :math: `M` is a scalar parameter.
+
+    The equation involves 4th order derivatives, so its weak form could not be handled with the standard Lagrange
+    finite element basis. However, the equation can be split in two second-order equations adding a second unknown
+    auxiliary field :math: `\mu`:
+
+    .. math::
+       \frac{\partial c}{\partial t} - \nabla \cdot M \nabla\mu  &= 0 \quad \textrm{in} \ \Omega, \\
+       \mu -  \frac{d f}{d c} + \lambda \nabla^{2}c &= 0 \quad \textrm{ in} \ \Omega.
+
+    In this way, it is possible to solve this equation using the standard Lagrange basis and, indeed, this
+    implementation uses this form.
+
+    :param c: main Cahn-Hillard field
+    :param c0: initial condition for the main Cahn-Hillard field
+    :param mu: auxiliary field for the Cahn-Hillard equation
+    :param mu0: initial condition for the auxiliary field
+    :param q: test function for c
+    :param v: test function for mu
+    :param dt: time step
+    :param theta: theta value for theta method
+    :param chem_potential: UFL form for the Cahn-Hillard potential
+    :param lmbda: energetic weight for the gradient of c
+    :param M: scalar parameter
+    :return: the UFL form of the Cahn-Hillard Equation
+    """
+    # Define form for mu (theta method)
+    mu_mid = (1.0 - theta) * mu0 + theta * mu
+
+    # chem potential derivative
+    dfdc = ufl.diff(chem_potential, c)
+
+    # define form
+    l0 = ((c - c0) / dt) * q * ufl.dx + M * ufl.dot(ufl.grad(mu_mid), ufl.grad(q)) * ufl.dx
+    l1 = mu * v * ufl.dx - dfdc * v * ufl.dx - lmbda * ufl.dot(ufl.grad(c), ufl.grad(v)) * ufl.dx
+    form = l0 + l1
+
+    # return form
+    return form
+
+
+def angiogenesis_form(c: dolfinx.fem.Function,
+                      c0: dolfinx.fem.Function,
+                      mu: dolfinx.fem.Function,
+                      mu0: dolfinx.fem.Function,
+                      v1: ufl.TestFunction,
+                      v2: ufl.TestFunction,
+                      af: dolfinx.fem.Function,
+                      parameters: Parameters = None,
+                      **kwargs):
+    r"""
+    Returns the UFL form for the Phase-Field model for angiogenesis reported by Travasso et al. (2011)
+    :cite:`Travasso2011a`.
+
+    The equation reads simply as the sum of a Cahn-Hillard term and a proliferation term (for further details see
+    the original paper):
+
+    .. math::
+       \frac{\partial c}{\partial t} = M \cdot \nabla^2 [\frac{df}{dc}\ - \epsilon \nabla^2 c]
+       + \alpha_p(T) \cdot c H(c)
+
+    Where :math: `c` is the unknown field representing the capillaries, and :
+
+    .. math:: f = \frac{1}{4} \cdot c^4 - \frac{1}{2} \cdot c^2
+
+    .. math::
+       \alpha_p(af) &= \alpha_p \cdot af_p \quad \textrm{if} \quad af>af_p \\
+                    &= \alpha_p \cdot af  \quad \textrm{if} \quad 0<af \le af_p \\
+                    & = 0 \quad \textrm{if} \quad af \le 0
+
+    In this implementation, the equation is split in two equations of lower order, in order to make the weak form
+    solvable using standard Lagrange finite elements:
+
+    .. math::
+       \frac{\partial c}{\partial t} &= M \nabla^2 \cdot \mu + \alpha_p(T) \cdot c H(c) \\
+       \mu &= \frac{d f}{d c} - \epsilon \nabla^{2}c
+
+    :param c: capillaries field
+    :param c0: initial condition for the capillaries field
+    :param mu: auxiliary field
+    :param mu0: initial condition for the auxiliary field
+    :param v1: test function for c
+    :param v2: test function  for mu
+    :param af: angiogenic factor field
+    :param parameters: simulation parameters
+    :return:
+    """
+    # get parameters
+    dt, epsilon, M, alpha_pc, T_p = _unpack_parameters_list(["dt", "epsilon", "M", "alpha_pc", "T_p"],
+                                                            parameters,
+                                                            kwargs)
+    # define theta
+    theta = 0.5
+
+    # define chemical potential for the phase field
+    c = ufl.variable(c)
+    chem_potential = ((c ** 4) / 4) - ((c ** 2) / 2)
+
+    # define total form
+    form_cahn_hillard = cahn_hillard_form(c, c0, mu, mu0, v1, v2, dt, theta, chem_potential, epsilon, M)
+    form_proliferation = vascular_proliferation_form(alpha_pc, af, T_p, c, v1)
+    form = form_cahn_hillard - form_proliferation
+
+    return form
